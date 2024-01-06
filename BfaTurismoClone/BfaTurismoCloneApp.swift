a7082cc030e713042426bfff8972d571e8b6c508
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-01-06 22:31:35 +0100

    Initial Commit

---
+++ BfaTurismoClone/BfaTurismoCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BfaTurismoCloneApp.swift
+//  BfaTurismoClone
+//
+//  Created by Eliezer Antonio on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BfaTurismoCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BfaTurismoClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BfaTurismoClone
+//
+//  Created by Eliezer Antonio on 06/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
